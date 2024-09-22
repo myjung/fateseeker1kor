8c943d6ee81056e364008bfd682fb4bd7a037b83
myjung <jmy1330@gmail.com>
2024-09-23 02:12:27 +0900

    update

---
+++ main.py
@@ -0,0 +1,69 @@
+import pathlib
+import logging
+import zipfile
+import os
+from datetime import datetime
+
+# pypi packages
+import toml
+import argparse
+
+# custom packages
+from patcher.extractor import FateSeeker1Patcher, FateSeeker1MetaInfo, FateSeeker1PatchHelper
+
+
+def main():
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Translate Fate Seeker 1")
+
+    # Add the arguments
+    parser.add_argument("-b", "--backup", action="store_true", help="Extract files")
+    parser.add_argument("-e", "--extract", action="store_true", help="Extract Keywords")
+    parser.add_argument("-p", "--patch", action="store_true", help="Make patch")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    # 로깅 설정
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.StreamHandler()],
+    )
+
+    setting_path = pathlib.Path("localconfig.toml")
+    if args.backup:
+        # Extract config files
+        FateSeeker1Patcher.backup_asset(setting_path)
+
+    if args.extract:
+        # Extract keywords
+        # patch_helper.extract_every_keywords_to_file("./data/extracted_strings.csv")
+        pass
+
+    if args.patch:
+        # # Make patch
+        # patch_helper.patch("./data/translated.csv", "./build/config")
+        # current_time = datetime.now().strftime("%y%m%d%H%M")
+        # zipf = zipfile.ZipFile(f"GulongPatch{current_time}.zip", "w", zipfile.ZIP_DEFLATED)
+        # zipf.write("readme-patchInfo.txt")
+
+        # # ./build 경로의 모든 파일을 /古龙风云录/AssetBundles/ 경로에 추가
+        # for root, dirs, files in os.walk("./build"):
+        #     for file in files:
+        #         # 파일의 전체 경로를 가져옵니다
+        #         full_path = os.path.join(root, file)
+        #         # zip 파일 내의 경로를 설정합니다
+        #         in_zip_path = os.path.join("古龙风云录/AssetBundles", os.path.relpath(full_path, "./build"))
+        #         # 파일을 zip 파일에 추가합니다
+        #         zipf.write(full_path, in_zip_path)
+
+        # # zip 파일을 닫습니다
+        # zipf.close()
+        pass
+
+    if not args.backup and not args.extract and not args.patch:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
---
+++ patcher/extractor.py
@@ -0,0 +1,94 @@
+from dataclasses import dataclass
+import toml as tomllib
+import pathlib
+from shutil import copyfile
+
+import UnityPy
+from UnityPy.enums import ClassIDType
+
+
+@dataclass
+class FateSeeker1MetaInfo:
+    # textfiles
+    localization = "assets/forassetbundles/textfiles/localization.csv"
+    # movie_other
+    opening = "assets/forassetbundles/movies/other/opening_chinese.bytes"
+    dlc_opening = "assets/forassetbundles/movies/other/dlcopening_chinese.bytes"
+
+
+class FateSeeker1Patcher:
+    @staticmethod
+    def backup_asset(
+        setting_path: pathlib.Path, target_path=pathlib.Path("./extracted_assets")
+    ) -> None:
+        settings = tomllib.load(setting_path)["local"]
+        game_path = pathlib.Path(settings["GAME_PATH"])
+        asset_path = game_path.joinpath(
+            "FateSeeker/FateSeeker_Data/StreamingAssets/StandaloneWindows64"
+        )
+        text_files = asset_path.joinpath("textfiles")
+        movie_other = asset_path.joinpath("movie_other")
+        copyfile(text_files, target_path.joinpath("textfiles.bak"))
+        copyfile(movie_other, target_path.joinpath("movie_other.bak"))
+
+    def __init__(self, extracted_asset_path: pathlib.Path) -> None:
+        # copy assets from backupfiles and Load
+        copyfile(
+            extracted_asset_path.joinpath("textfile.bak"),
+            extracted_asset_path.joinpath("textfiles"),
+        )
+        copyfile(
+            extracted_asset_path.joinpath("movie_other.bak"),
+            extracted_asset_path.joinpath("movie_other"),
+        )
+        self.textfiles = UnityPy.load(str(extracted_asset_path.joinpath("textfiles")))
+        self.movie_other = UnityPy.load(
+            str(extracted_asset_path.joinpath("movie_other"))
+        )
+
+    def get_text(self, path: str) -> str:
+        asset = self.textfiles.container[path]
+        if not asset.type == ClassIDType.TextAsset:
+            raise Exception("Not Text Asset")
+        t = ""
+        try:
+            t = asset.read().text
+        except UnicodeDecodeError:
+            t = bytes(asset.read().script).decode("big5hkscs")
+        if t[:1] == "\ufeff":
+            return t[1:]
+        else:
+            return t
+
+    def set_text(self, path: str, text: str) -> None:
+        asset = self.textfiles.container[path]
+        if not asset.type == ClassIDType.TextAsset:
+            raise Exception("Not Text Asset")
+        data = asset.read()
+        data.script = text.encode("utf-8")
+        data.save()
+
+    def save_asset(self, path: str) -> None:
+        with open(path, mode="wb") as f:
+            f.write(self.textfiles.file.save())
+
+
+class FateSeeker1PatchHelper:
+    def __init__(self, patcher: FateSeeker1Patcher) -> None:
+        self.patcher = patcher
+
+    def extract_text(self) -> list:
+        # extract text assets from asset files
+        pass
+
+    def patch(self, input_keywords_path: str, output_path: str) -> None:
+        # replace text with translated text
+        pass
+
+
+if __name__ == "__main__":
+    current_file_path = pathlib.Path(__file__)
+    local_config_path = current_file_path.parent.joinpath("..", "localconfig.toml")
+    # ph = PatchHelper(PatchTargets, patcher)
+    # ph.patch("translated2403082200.csv","config")
+    # ph.extract_every_keywords_to_file("2403082000.csv")
