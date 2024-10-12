7dcbfe952e16403df004ca11eb03353734704207
myjung <jmy1330@gmail.com>
2024-10-13 07:03:49 +0900

    update

--- patcher/translator.py
+++ patcher/translator.py
@@ -108,7 +108,7 @@ async def translate_text(text, api_id: str, api_key: str, glossary_key: str = ""
 
 
 class Claude3Api:
-    def __init__(self, api_key: str, max_tokens: int = 1001, temperature: int = 0):
+    def __init__(self, api_key: str, max_tokens: int = 8192, temperature: int = 0):
         self.client = anthropic.Anthropic(api_key=api_key)
         self.max_tokens = max_tokens
         self.temperature = temperature
@@ -117,7 +117,7 @@ class Claude3Api:
         self, prompt: str, first_answer: str, text: str, input_max_tokens: int = None, input_temperature: int = None
     ):
         message = self.client.messages.create(
-            model="claude-3-opus-20240229",
+            model="claude-3-5-sonnet-20240620",
             max_tokens=input_max_tokens or self.max_tokens,
             temperature=input_temperature or self.temperature,
             messages=[
